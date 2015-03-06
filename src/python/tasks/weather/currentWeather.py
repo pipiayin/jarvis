bef1039ad71e7ce4bf75445c9fd06c1b248f64ac
Tao-Sheng Chen (RD-TW) <tao-sheng_chen@trend.com.tw>
2015-03-06 11:03:20 +0800

    a function for weather

--- src/python/tasks/weather/currentWeather.py
+++ src/python/tasks/weather/currentWeather.py
@@ -6,15 +6,30 @@ import traceback
 import httplib2
 import json
 
+
 httpclient = httplib2.Http()
 APIID='f3e2c3a8720782b6d4b0c3c177772cfa'
 APIURL='http://api.openweathermap.org/data/2.5/weather?'
 
-
-if __name__ == '__main__':
-    params_string = 'q=Taipei,tw&APIID='+APIID
-    params_string = 'lon=121.53&lat=25.05&APIID='+APIID
+def getWeatherViaLocation(lon,lat):
+    params_string = 'lon={0}&lat={1}&APIID={2}'.format(lon,lat,APIID)
     url = APIURL + params_string
     resp, content = httpclient.request(url,"GET",None)
-    print resp
-    print content
+    result = eval(content)
+    return result
+
+if __name__ == '__main__':
+    try:
+#        params_string = 'q=Taipei,tw&APIID='+APIID
+#        params_string = 'lon=121.53&lat=25.05&APIID='+APIID
+#        url = APIURL + params_string
+#        resp, content = httpclient.request(url,"GET",None)
+#        import XXX
+        print getWeatherViaLocation(121.53,25.05)
+
+    except(ImportError):
+        print("try to install other things")
+    except:
+        print("unknow issues")
+
+
