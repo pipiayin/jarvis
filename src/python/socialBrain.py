e91429b22a916158bc5e7c2f0c098c8fd0d5b930
chen taosheng <dougchen@talent-service.com>
2016-06-14 16:33:27 +0800

    facebook app

---
+++ src/python/socialBrain.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+import jieba
+import jieba.posseg as pseg
+
+
+
+from brain import Brain
+
+class SocialBrain(Brain):
+
+
+    def load(self, info):
+        print("load information")
+
+    def think(self, userSession):
+        print(userSession)
+
+
+if __name__ == '__main__':
+
+    fbBrain = SocialBrain()
+    
+    jieba.set_dictionary('data/dict.txt.big')
+
+    s = open('secret.so')
+    client_secret= s.readline().strip()
+    s.close()
+
+    userSession = {"name":"","lastWords":"", "lastString":""}
+    userReq = u'照片不錯'
+    words = pseg.cut(userReq)
+    userSession["lastWords"]  = words
+    userSession["lastString"]  = userReq
+
+    fbBrain.think(userSession)
+
+    app_id = '1091008854278872'
+    r = requests.get('https://graph.facebook.com/oauth/access_token?grant_type=client_credentials&client_id='+app_id+'&client_secret='+client_secret)
+    access_token = r.text.split('=')[1]
+    print(access_token)
+    r = requests.get('https://graph.facebook.com/v2.6/1091008854278872/accounts',params={'access_token':access_token})
+    print(r.text)
+
+
