a0eeeda310e78aafb437154cb1f099140f53c682
Tao-Sheng <tao-sheng_chen@trend.com.tw>
2016-08-31 14:34:56 +0800

    simple health kb

--- chatbot/src/retrieveData.py
+++ chatbot/src/retrieveData.py
@@ -3,61 +3,110 @@
 
 import requests
 import lxml.html as lh
-from StringIO import StringIO
+#from StringIO import StringIO
+from io import StringIO
 import re
 
 from lxml import etree
 
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+from datetime import datetime
+import sys
+import csv
+
+#host = 'search-tsai-t5aqxu4dppacep22fq5b4uvj6m.us-east-1.es.amazonaws.com'
+host = 'search-sandyai-mdmcmay32zf36sgmk66tz2v454.us-east-1.es.amazonaws.com'
+awsauthfile = '/root/.aws/credentials'
+aws_access_key_id = '' 
+aws_secret_access_key = ''
+
+with open(awsauthfile) as f:
+    content = f.readlines()
+    for line in content:
+        if 'aws_access_key_id' in line :
+            aws_access_key_id = line.split("=")[1].strip()
+        if 'aws_secret_access_key' in line :
+            aws_secret_access_key = line.split("=")[1].strip()
+
+awsauth = AWS4Auth(aws_access_key_id, aws_secret_access_key, 'us-east-1', 'es')
+
+
+es = Elasticsearch(
+    hosts=[{'host': host, 'port': 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
 
 
 
-f = open('out1.csv','a')
-for i in range(2,10000):
+
+def insertEs(doc):
+    print('start to insert------------')
+    print(doc['dr'])
+    res = es.index(index="health", doc_type='web',  body=doc)
+    print(res)
+    print("inserted ------------")
+
+
+
+def basicClean(amsg):
+    amsg= amsg.encode('utf-8').strip()
+    #print(amsg)
+   # amsg= amsg.encode('utf-8').strip()
+    amsg = amsg.replace('《','')
+    amsg = re.sub(r'\n', '', amsg)
+    amsg = re.sub(r'\r', '', amsg)
+   # amsg = re.sub(r'\r\n', '', amsg)
+    amsg = re.sub(r',', '', amsg)
+    amsg = re.sub(r'《', '', amsg)
+    amsg = re.sub(r' ', '', amsg)
+    r = amsg
+    print(r)
+    print('done cleaning')
+    return r
+
+f = open('health1.csv','a')
+for i in range(10121,20000):
     try:
         url='http://sp1.hso.mohw.gov.tw/doctor/All/ShowDetail.php?q_no='
         r = requests.get(url+str(i))
 #    print(r.status_code)
         htmlstr = r.text.encode('latin1', 'ignore').decode('big5')
         if htmlstr.count(u'不存在</h1>'):
-#            print('ignore '+str(i))
+            print('ignore '+str(i))
             continue
     #doc=lh.parse(htmlstr)
         parser = etree.HTMLParser()
+        sio = StringIO(htmlstr)
         tree = etree.parse(StringIO(htmlstr), parser)
         question = tree.find(".//li[@class='ask']")
         allq =""
         for t in question.itertext():
             allq = allq + t
-        print("---")
-        print(allq)
-        print("---")
         dr = tree.find(".//li[@class='doctor']").text
-#    dr.split(",")[0]
+#    dr.nsplit(",")[0]
         ans = tree.find(".//li[@class='ans']")
         alla = ""
         for t in ans.itertext():
             t.replace("\n","")
             alla = alla+t
-
-        allq = re.sub(r'\n', '', allq)
-        alla = re.sub(r'\n', '', alla)
-        dr = re.sub(r'\n', '', dr)
     
-        allq = re.sub(r',', '', allq)
-        alla = re.sub(r',', '', alla)
-        dr = re.sub(r',', '', dr)
-        print('===========')    
-        allq = allq.encode('utf-8').strip()
-        alla = alla.encode('utf-8').strip()
-        dr = dr.encode('utf-8').strip()
-        print('q:'+allq)
-        print('a:'+alla)
-        print('dr:'+dr)
-        print("======")
-        oneresult = allq+","+alla+","+dr
-    
-#        f.write(oneresult+"\n")
+        allq = basicClean(allq)
+        alla = basicClean(alla)
+        dr = basicClean(dr)
+        oneresult = {'a':alla,'q':allq,'dr':dr}
+#        insertEs(oneresult)
+        f.write(allq+ ","+alla+","+dr)
+        f.write("\n")
     except:
+        print(sys.exc_info()[0])
         pass 
+
+es.indices.refresh(index="health")
 #    print(oneresult)
 #    print(r.text.encode('utf-8'))
+f.flush()
+f.close()
---
+++ chatbot/src/uploadKbToEs_health.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+from datetime import datetime
+import fileinput
+
+import sys
+import csv
+
+#host = 'search-tsai-t5aqxu4dppacep22fq5b4uvj6m.us-east-1.es.amazonaws.com'
+host = 'search-sandyai-mdmcmay32zf36sgmk66tz2v454.us-east-1.es.amazonaws.com'
+awsauthfile = '/root/.aws/credentials'
+aws_access_key_id = '' 
+aws_secret_access_key = ''
+
+with open(awsauthfile) as f:
+    content = f.readlines()
+    for line in content:
+        if 'aws_access_key_id' in line :
+            aws_access_key_id = line.split("=")[1].strip()
+        if 'aws_secret_access_key' in line :
+            aws_secret_access_key = line.split("=")[1].strip()
+
+awsauth = AWS4Auth(aws_access_key_id, aws_secret_access_key, 'us-east-1', 'es')
+
+
+es = Elasticsearch(
+    hosts=[{'host': host, 'port': 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
+print(es.info())
+
+print("to load...")
+
+with open(sys.argv[1]) as f:
+    for tmpLine in f:
+        line = tmpLine.strip().split(",")
+        q = line[0].strip()
+        a = line[1].strip()
+        dr = line[2].strip()
+        item={u'q':q, u'a':a, u'dr':dr}
+        # to make sure similar table always has itself
+
+        print(item)
+        res = es.index(index="health", doc_type='web',  body=item)
+
+#            allkb[oriKey]['similar'] = similarList
+
+
+#for k in allkb:
+#    print(allkb[k])
+#    #res = es.index(index="testi", doc_type='fb',  body=allkb[k])
+
+es.indices.refresh(index="health")
+print("======= all upload ======")
+
+
+q = {
+      "query" :{
+      "multi_match" : {
+        "query": u'心悸',
+        "fields": [ "q", "a" ]
+      }
+      }
+ }
+
+
+
+res = es.search(index="health", body=q)
+print(res)
+print("Got %d Hits:" % res['hits']['total'])
+for h in res['hits']['hits']:
+    print(h)
+
+
