17e68084c20ccbd76bcddcc0599e3f1b6dc555fd
Tao-Sheng <tao-sheng_chen@trend.com.tw>
2016-08-31 10:43:02 +0800

    add rm

--- chatbot/src/deployToAWS_TS.sh
+++ chatbot/src/deployToAWS_TS.sh
@@ -1,4 +1,5 @@
 # zip , s3 upload, aws lambda commandline update
-zip ailambda.zip -r data/ dynamoKB.py jieba/ lambda_ai.py socialBrain.py basickb.csv requests/  pttChat.py basickb.csv wikiChat.py esKB.py elasticsearch/ urllib3
+rm ailambda.zip
+zip ailambda.zip -r data/ dynamoKB.py jieba/ lambda_ai.py socialBrain.py basickb.csv requests/  pttChat.py basickb.csv wikiChat.py esKB.py elasticsearch/ urllib3 requests_aws4auth/ credentials
 aws s3 cp ailambda.zip s3://sandyai/
 aws lambda update-function-code --function-name sandyaipost --s3-bucket sandyai --s3-key ailambda.zip
