d395495f9e16a0d1248f23d7c9424e9cfcfc5fef
Tao-Sheng <tao-sheng_chen@trend.com.tw>
2016-09-02 10:35:09 +0800

    to delete certain index

--- chatbot/src/deleteIndex.py
+++ chatbot/src/deleteIndex.py
@@ -4,9 +4,24 @@
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 from datetime import datetime
+import sys
+import csv
 
+#host = 'search-tsai-t5aqxu4dppacep22fq5b4uvj6m.us-east-1.es.amazonaws.com'
 host = 'search-sandyai-mdmcmay32zf36sgmk66tz2v454.us-east-1.es.amazonaws.com'
-awsauth = AWS4Auth('AKIAISA4GAR6JJLA4QNQ', 'towlfkviLmA4QMpMmS3mhBNNdWbsg6yQqyMoUciF', 'us-east-1', 'es')
+awsauthfile = '/root/.aws/credentials'
+aws_access_key_id = '' 
+aws_secret_access_key = ''
+
+with open(awsauthfile) as f:
+    content = f.readlines()
+    for line in content:
+        if 'aws_access_key_id' in line :
+            aws_access_key_id = line.split("=")[1].strip()
+        if 'aws_secret_access_key' in line :
+            aws_secret_access_key = line.split("=")[1].strip()
+
+awsauth = AWS4Auth(aws_access_key_id, aws_secret_access_key, 'us-east-1', 'es')
 
 
 es = Elasticsearch(
@@ -18,21 +33,14 @@ es = Elasticsearch(
 )
 print(es.info())
 
-#es.indices.refresh(index="testi")
+
+#es.indices.create(index="health")
+#print("======= all upload ======")
 
 
-#q = {
-#  "query" :{
-#  "match" : { '':''
-#  }
-#  }
-#}
+indexname = sys.argv[1]
 
-#res = es.search(index="testi", body=q)
+es.indices.delete(index=indexname)
+es.indices.create(index=indexname)
 
-#print(dir(es))
-#print("Got %d Hits:" % res['hits']['total'])
-#print(res['hits']['hits'])
-print('-----')
-es.indices.delete('testi')
 
