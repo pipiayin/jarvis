7aa6b4e17fc0a89e29414c98054b04b7635d2677
Tao-Sheng <tao-sheng_chen@trend.com.tw>
2017-05-26 13:42:48 +0800

    deploy taipeibus.zip to lambda

--- taipeiBusTool/deploy.sh
+++ taipeiBusTool/deploy.sh
@@ -1,6 +1,7 @@
 # zip , s3 upload, aws lambda commandline update
 #zip aiBrain.zip -r data/ jieba/ lambda_brain.py socialBrain.py basickb.csv requests/  pttChat.py basickb.csv wikiChat.py esKB.py elasticsearch/ urllib3 requests_aws4auth/ credentials esHealth.py esBible.py linecre.so awsconfig.py credentials_ai nocheckin.py genericKB.py blackList.py
-#zip aiLearn.zip -r  elasticsearch/ urllib3 requests/ requests_aws4auth/ credentials linecre.so awsconfig.py credentials_ai nocheckin.py lambda_learn.py genericKB.py blackList.py
-aws --profile ailine s3 cp taipeibus.html s3://taipeibus/
-aws --profile ailine s3 cp all.js s3://taipeibus/
-#aws --profile ailine lambda update-function-code --function-name linepost --s3-bucket sandyai2 --s3-key ailine.zip
+zip taipeibus.zip -r requests/ lambda_learn.py rtBus.py
+#aws --profile ailine s3 cp taipeibus.html s3://taipeibus/
+aws --profile ailine s3 cp taipeibus.zip s3://taipeibus/
+aws --profile ailine s3 cp taipeibus.zip s3://sandyai2/
+aws --profile ailine lambda update-function-code --function-name taipeibus --s3-bucket sandyai2 --s3-key taipeibus.zip
