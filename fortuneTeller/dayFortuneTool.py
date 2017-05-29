6c3dfd9603d4a19cfb785f2fa8b07be19db6ca93
Tao-Sheng <tao-sheng_chen@trend.com.tw>
2017-05-29 09:57:54 +0800

    simple tool to tell birthday

---
+++ fortuneTeller/dayFortuneTool.py
@@ -0,0 +1,36 @@
+
+import requests
+import sys
+from hanziconv import HanziConv
+
+DAY_TELLER_URL = "http://www.53bang.com/shengri/{}.html"
+
+def getDayContext(allText):
+    text = allText.split("name_banner")[1].split("<P>")[1].split("</P>")[0]
+    text = HanziConv.toTraditional(text)
+    text = text.replace(u'齣生',u'出生')
+    return text
+
+def getTitleContext(allText):
+    text = allText.split("name_ctitle")[1].split("<h2>")[1].split("</h2>")[0]
+    text = HanziConv.toTraditional(text)
+    return text
+
+def getAllContext(birthday):
+    getUrl = DAY_TELLER_URL.format(day)
+    print(getUrl)
+    r = requests.get(getUrl)
+    return r.text
+
+def tellTheDay(birthday):
+    allText = getAllContext(day)
+    text = getDayContext(allText)
+    titleText = getTitleContext(allText)
+    return (titleText + "\n" + text)
+
+
+if __name__ == '__main__':
+    day = sys.argv[1]
+    print(tellTheDay(day))
+  
+
