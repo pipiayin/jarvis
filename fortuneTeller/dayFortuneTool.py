7c24d150f3b1afae27390b864f079d6e234897d3
Tao-Sheng <tao-sheng_chen@trend.com.tw>
2017-06-02 10:42:40 +0800

    quick fix some simplfied translate issue

--- fortuneTeller/dayFortuneTool.py
+++ fortuneTeller/dayFortuneTool.py
@@ -6,10 +6,24 @@ from datetime import datetime
 
 DAY_TELLER_URL = "http://www.53bang.com/shengri/{}.html"
 
+def cleanHanziError(text):
+    errorMap = { u'齣生':u'出生',
+                 u'方麵':u'方面',
+                 u'特彆':u'特別',
+                 u'傢':u'家',
+                 u'波摺':u'波折',
+                 u'纔能':u'才能',
+                 u'齣外':u'出外',
+                 u'睏':u'困',
+                 u'死彆':u'死別'
+               }
+    for k in errorMap:
+        text = text.replace(k,errorMap[k])
+    return text
 def getDayContext(allText):
     text = allText.split("name_banner")[1].split("<P>")[1].split("</P>")[0]
     text = HanziConv.toTraditional(text)
-    text = text.replace(u'齣生',u'出生')
+    text = cleanHanziError(text)
     return text
 
 def getTitleContext(allText):
