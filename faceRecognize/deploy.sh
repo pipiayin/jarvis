193bedf7dd89c8a85a9eb5f3e8eb90b4cf2de34d
Tao-Sheng <tao-sheng_chen@trend.com.tw>
2017-07-24 15:34:04 +0800

    simple fr lambda

---
+++ faceRecognize/deploy.sh
@@ -0,0 +1,15 @@
+# To deploy lambda on AWS
+rm facerecognize.zip
+
+zip facerecognize.zip -r lambda_facerecognize.py  requests/ 
+
+aws --profile ailine s3 cp facerecognize.zip s3://sandyai2/
+
+aws --profile ailine lambda create-function --function-name facerecognize --runtime python3.6 --role "arn:aws:iam::740157263594:role/sandyairun" --handler lambda_facerecognize.lambda_handler --timeout 120 --code "S3Bucket=sandyai2,S3Key=facerecognize.zip"
+#aws --profile ailine lambda create-function --function-name astro --runtime python3.6 --role "arn:aws:iam::740157263594:role/sandyairun" --handler lambda_astro.lambda_handler --timeout 59 --code "S3Bucket=sandyai2,S3Key=astro.zip"
+#aws --profile ailine lambda create-function --function-name dayfortune --runtime python3.6 --role "arn:aws:iam::740157263594:role/sandyairun" --handler lambda_dayfortune.lambda_handler --timeout 59 --code "S3Bucket=sandyai2,S3Key=dayfortune.zip"
+
+aws --profile ailine lambda update-function-code --function-name facerecogniz --s3-bucket sandyai2 --s3-key facerecognize.zip
+
+
+
---
+++ faceRecognize/lambda_facerecognize.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function 
+import json
+import boto3
+import sys
+import datetime
+import requests
+from nocheckin import XLineToken
+
+
+lambda_client = boto3.client('lambda')
+
+def uploadLineImageToS3(uid, imageId, bucket='sandyiface'):
+    headers = {"Content-type": "application/json; charset=utf-8","Authorization" : "Bearer "+XLineToken}
+
+    imageUrl = 'https://api.line.me/v2/bot/message/{}/content'.format(imageId)
+    r = requests.get(imageUrl, headers=headers, stream=True)
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket)
+    objkey = uid + "_" + imageId
+    obj = bucket.Object(objkey)
+    with r.raw as data:
+        obj.upload_fileobj(data)
+
+    return objkey
+
+def recognizeCelebrities(objkey,bucket='sandyiface'):
+
+    rclient = boto3.client('rekognition')
+    response = rclient.recognize_celebrities(
+    Image = {
+            'S3Object': {
+            'Bucket': bucket,
+            'Name': objkey }
+           }
+    )
+    return response
+    
+
+
+def analysisFaceRec(response):
+    #TODO improve message
+    msg = ""
+    if len(response['CelebrityFaces']) == 0:
+        msg = u'這個相片裡的人都不太有名...'
+
+    if len(response['CelebrityFaces']) > 0 :
+        for f in response['CelebrityFaces']:
+            msg = u'這張相片中的人 似乎和 {} 有 {}的相似度 '.format(f['Name'], f['MatchConfidence'])
+            msg = msg + "\n"
+            
+            if len(f['Urls']) > 0:
+                msg = msg+ " 請參考下列網址:\n"
+                for url in f['Urls']:
+                    msg = msg + url+"\n"
+               
+    print(msg)
+    return msg
+
+def lambda_handler(even, context):
+    try:
+        print("-----In faceRecognize---")
+        # even format: {"uid": "botid": , "callback":"lineResponse", "imageId":"image_id_from_line"}
+        # TODO: ASSUMPTION, all callback assume go for lineResponse
+        # TODO: ASSUMPTION, all image store to the same bucket sandyiface, with name uid_imageid
+        print(even)
+        uid = '' 
+        imageId =  ''
+        if 'uid' in even :
+            uid = even['uid']
+        else:
+            return 
+        if 'imageId' in even :
+            imageId = even['imageId']
+        else:
+            return 
+
+        objKeyString = uploadLineImageToS3(uid, imageId)
+        cresponse = recognizeCelebrities(objKeyString)
+       
+        msg = analysisFaceRec(cresponse)
+        
+        toLineResponse={'uid':uid, 'msg':msg}
+        if msg == '':
+            return
+
+        lresponse = lambda_client.invoke(
+             FunctionName='lineResponse',
+             InvocationType='Event',
+             LogType='None',
+             ClientContext='string',
+             Payload=json.dumps(toLineResponse),
+         )
+
+        return 
+    except:
+        print(even)
+        print(sys.exc_info()[0])
+        return "something wrong"
+   
+
+if __name__ == '__main__':
+    print("TODO: simple test script")
+    imageId = u'6435122147042'
+    imageId = u'6435265574375'
+    imageId = u'6435271838359'
+    imageId = u'6435322417921'
+    userId =  u'Uc9b95e58acb9ab8d2948f8ac1ee48fad'
+    even = {'uid':userId, 'imageId':imageId}
+    lambda_handler(even, None)
