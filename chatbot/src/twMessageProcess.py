8509aaa4c7183b3598499197a7de80c56ba7b78e
Tao-Sheng <tao-sheng_chen@trend.com.tw>
2017-08-09 15:46:39 +0800

    simple way to provide bots intent

---
+++ chatbot/src/twMessageProcess.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import jieba
+import jieba.posseg as pseg
+
+jieba.load_userdict('data/dict.txt.big')
+
+def getIntent(msg):
+    msg = msg.strip()
+    tWord = ''
+    verb = ''
+    entity = []
+    timing = []
+    location = ''
+ 
+    if len(msg) <= 4 :
+        entity.append(msg)
+        verb = msg
+    
+
+    words = pseg.cut(msg)
+
+    pnv = False
+    tmpN = ''
+    for word in words:
+        print(word.flag+" "+word.word)
+        if word.flag in  ['nz','nt','n']:
+            tmpN = tmpN + word.word
+            entity.append(tmpN)
+            pnv = True
+            tmpN = word.word
+        elif word.flag in  ['v','vi','vn']:
+            verb = word.word
+        elif word.flag in ['t','tg']:
+            timing.append(word.word)
+        elif word.flag in ['f','ns']:
+            location = word.word
+        else:
+            tmpN = ''
+            pnv = False
+
+    intent = { 'intent':verb, 'timings':timing, 'location':location, 'entities': entity}
+    print(intent)
+    return intent
+
+
+if __name__ == '__main__' :
+    import sys
+    msg = sys.argv[1]
+    print('process msg:'+msg)
+    getIntent(msg)
+    
