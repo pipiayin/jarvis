779eb5cff49aca364d2fd9cc32ef638314d3bb29
tao-sheng_chen <tao-sheng_chen@trend.com.tw>
2017-09-01 11:19:35 +0800

    simple w2v model

--- pixnetNews/tryModel.py
+++ pixnetNews/tryModel.py
@@ -1,20 +1,27 @@
-
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 from gensim.models import word2vec
 import sys
 
-def main(modelFile):
+def main(modelFile, msg):
 
    # To load a model.
     model = word2vec.Word2Vec.load(modelFile)
     print(type(model.wv))
     print(dir(model.wv))
+    print(model.wv.most_similar(msg))
+    #i = 1
+    #for k in model.wv.vocab.keys():
+    #    print(i)
+    #    print(k)
+    #    i += 1
+       
     
   
 
 
 if __name__ == '__main__':
     modelFile = sys.argv[1]
-    main(modelFile)
+    msg = sys.argv[2]
+    main(modelFile, msg)
