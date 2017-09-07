beb70b911b61c30b6a4058d12399f9c769f15fb1
Tao-Sheng <tao-sheng_chen@trend.com.tw>
2017-09-07 16:45:30 +0800

    add excludes in aiConfig

--- chatbot/src/aiProfile.py
+++ chatbot/src/aiProfile.py
@@ -12,6 +12,31 @@ def getMatchesFromIntent(intent,profile=AI_PROFILE):
             if len(intent['msg']) not in range(p['lenRange'][0], p['lenRange'][1]):
                 continue
         
+        hasExclude = False
+        if 'excludes' in p:
+            for e in p['excludes']:
+                for (oneC,oneV) in e:
+                    if oneC == 'intent':
+                        if intent[oneC] == oneV :
+                            hasExclude = True
+                            break
+                    if oneC == 'timings':
+                        if  oneV in intent[oneC] :
+                            hasExclude = True
+                            break
+                    if oneC == 'entity':
+                        if oneV in intent['entities']:
+                            hasExclude = True
+                            break
+                    if oneC == 'location':
+                        if oneV  in intent[oneC]:
+                            hasExclude = True
+                            break
+                if hasExclude == True:
+                    break
+
+        if hasExclude == True:
+            break
 
         for c in p['criteries']:
             toMatch = len(c)
@@ -41,12 +66,14 @@ if __name__ == '__main__' :
     intent = {'intent': '學習', 'timings': [], 'entities': ['你', '怎麼'], 'msg': '你是怎麼學習的', 'location': ''}
     intent = {'entities': ['如何', '聊天機器人'], 'msg': '如何製作聊天機器人', 'timings': [], 'intent': '製作', 'location': ''}
     intent = {'timings': ['生日'], 'oriCut': [('r', '你'), ('uj', '的'), ('t', '生日')], 'intent': '', 'msg': '你的生日', 'entities': ['你的生日', '你'], 'location': ''}
+    intent = {'oriCut': [('r', '你'), ('d', '不'), ('v', '喜歡'), ('n', '做什麼')], 'intent': '喜歡', 'entities': ['做什麼', '你', '不'], 'timings': [], 'location': '', 'msg': '你不喜歡做什麼'}
 #    intent = {'msg': '今天天氣很不錯唷 你覺得啦', 'intent': '覺得', 'oriCut': [('i', '今天天氣'), ('zg', '很'), ('a', '不錯'), ('x', '唷'), ('x', ' '), ('r', '你'), ('v', '覺得'), ('y', '啦')], 'timings': [], 'entities': ['你', '不錯'], 'location': ''}
 
     resp = getMatchesFromIntent(intent)
     print(resp)
     import random
-    res = random.choice(random.choice(resp)['res'])
-    print(res)
+    if len(resp) > 0:
+        res = random.choice(random.choice(resp)['res'])
+        print(res)
 
 
